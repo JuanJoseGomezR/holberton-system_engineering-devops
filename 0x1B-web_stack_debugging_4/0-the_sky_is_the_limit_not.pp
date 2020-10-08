ecf6ed5f63ad7080f0c018fa128d2e173850a5f3
JuanJoseGomezR <juangomez0149@gmail.com>
2020-10-08 16:21:38 +0000

    [Fixed] Puppet-Lint Indentation

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # change amount of requests limit
 exec { 'increase-ULIMIT':
-	  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3000\"/g" /etc/default/nginx',
-		    path    => ['/bin']
+    command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3000\"/g" /etc/default/nginx',
+    path    => ['/bin']
 }
 exec { 'restart-nginx':
-	  command  => 'service nginx restart',
-		     provider => shell,
-		       require  => Exec['increase-ULIMIT']
+    command  => 'service nginx restart',
+    provider => shell,
+    require  => Exec['increase-ULIMIT']
 }
 
